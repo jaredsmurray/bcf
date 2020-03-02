88262e2f434428288dd1e5dd89f572a5edf5cf08
pmariani <pmariani@mathematica-mpr.com>
2020-03-02 11:47:47 -0500

    updating package

--- examples/update_package.sh
+++ examples/update_package.sh
@@ -6,6 +6,10 @@ Rscript.exe -e 'pkgbuild::compile_dll()'
 Rscript.exe -e 'devtools::document()'
 R CMD INSTALL --no-multiarch --with-keep.source ../bcf-1
 
+# pkgdown::build_site()
+# devtools::check()
+# rhub::check()
+
 # Rscript.exe examples/simple_example.R
 
 # Rscript.exe examples/test_pred2.R
